94249110bfe284d3cf43f9b0ae71c9c696ef096e
Goutye <gaut.b@sfr.fr>
2015-02-18 19:10:03 +0100

    SpriteAnimation: full implementation

--- src/EasyLD.lua
+++ src/EasyLD.lua
@@ -5,6 +5,7 @@ local utf8 = require 'utf8'
 local EasyLD = {}
 EasyLD.tileset = require 'Tileset'
 EasyLD.map = require 'Map'
+EasyLD.spriteAnimation = require 'SpriteAnimation'
 
 EasyLD.matrix = require 'Matrix'
 EasyLD.vector = require 'Vector'
@@ -70,5 +71,18 @@ function EasyLD:update(dt)
 end
 
 EasyLD.load = loadAPI
+
+function string:split(delimiter)
+	local result = { }
+	local from = 1
+	local delim_from, delim_to = string.find( self, delimiter, from)
+	while delim_from do
+		table.insert(result, string.sub(self, from , delim_from-1))
+		from = delim_to + 1
+		delim_from, delim_to = string.find(self, delimiter, from)
+	end
+	table.insert(result, string.sub( self, from ))
+	return result
+end
 
 return EasyLD
